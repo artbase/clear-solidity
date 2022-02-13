ca6daa1473db4aa3acf18945cc964c338a5649a5
art_base <bartosz.zaremba@live.com>
2022-02-13 10:54:26 +0100

    Added events, enum in solidty and file with helpful links

--- events/contracts/SimpleStorage.sol
+++ events/contracts/SimpleStorage.sol
@@ -2,10 +2,15 @@
 pragma solidity >=0.4.21 <8.10.0;
 
 contract SimpleStorage {
+  enum NumberSize { SMALL, MEDIUM, LARGE };
+
   uint storedData;
+
+  event HowManyStored(uint count);
 
   function set(uint x) public {
     storedData = x;
+    emit HowManyStored(x);
   }
 
   function get() public view returns (uint) {
